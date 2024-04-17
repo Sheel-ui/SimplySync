cb30cb09d990f86bca1d049f70ab74d0a4e94f24
Sheel Taskar <sheeltaskar64@gmail.com>
2024-04-17 00:24:42 -0400

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
+import Login from './components/Login/Login';
+import Setup from './components/Setup/Setup';
 import Navbar from './components/Navbar/Navbar';
 import Controls from './components/Controls/Controls';
 import ListSearch from './components/ListSearch/ListSearch';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-          <Navbar></Navbar>
+    <Router>
+      <div className="App">
+        <div className="container">
+        <Navbar></Navbar>
           <div className="content">
-              <Controls />
-              <ListSearch />
-          </div>
+          <Controls />
+          <Switch>
+            <Route exact path='/'>
+              <ListSearch></ListSearch>
+            </Route>
+                <Route exact path='/landing'>
+                <Landing />
+            </Route>
+            <Route path='/login'>
+                <Login />
+            </Route>
+            <Route path='/setup'>
+                <Setup />
+            </Route>
+          </Switch>
+          </div>        
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
